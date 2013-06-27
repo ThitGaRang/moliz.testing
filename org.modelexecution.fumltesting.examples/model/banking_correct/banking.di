3fe7b264de4b7ccda43a1a47d8a8c3cb3efb8a86
Tanja Mayerhofer <mayerhofer@big.tuwien.ac.at>
2013-06-27 10:06:49 +0200

    Adds example project with ATM example

---
+++ org.modelexecution.fumltesting.examples/model/banking_correct/banking.di
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="ASCII"?>
+<di:SashWindowsMngr xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:di="http://www.eclipse.org/papyrus/0.7.0/sashdi">
+  <pageList>
+    <availablePage>
+      <emfPageIdentifier href="banking.notation#_Ez3P8Nw9EeKn2ranrBSLLA"/>
+    </availablePage>
+    <availablePage>
+      <emfPageIdentifier href="banking.notation#_9Vw0wNw-EeKn2ranrBSLLA"/>
+    </availablePage>
+    <availablePage>
+      <emfPageIdentifier href="banking.notation#_sAlU0NxCEeKn2ranrBSLLA"/>
+    </availablePage>
+    <availablePage>
+      <emfPageIdentifier href="banking.notation#_-SY4QNxCEeKn2ranrBSLLA"/>
+    </availablePage>
+    <availablePage>
+      <emfPageIdentifier href="banking.notation#_yIRvoN5iEeKtmuWA2xqcdw"/>
+    </availablePage>
+    <availablePage>
+      <emfPageIdentifier href="banking.notation#_k7GCMN6aEeKSKuyNBSeryQ"/>
+    </availablePage>
+  </pageList>
+  <sashModel currentSelection="//@sashModel/@windows.0/@children.0">
+    <windows>
+      <children xsi:type="di:TabFolder">
+        <children>
+          <emfPageIdentifier href="banking.notation#_Ez3P8Nw9EeKn2ranrBSLLA"/>
+        </children>
+        <children>
+          <emfPageIdentifier href="banking.notation#_sAlU0NxCEeKn2ranrBSLLA"/>
+        </children>
+        <children>
+          <emfPageIdentifier href="banking.notation#_-SY4QNxCEeKn2ranrBSLLA"/>
+        </children>
+        <children>
+          <emfPageIdentifier href="banking.notation#_yIRvoN5iEeKtmuWA2xqcdw"/>
+        </children>
+        <children>
+          <emfPageIdentifier href="banking.notation#_9Vw0wNw-EeKn2ranrBSLLA"/>
+        </children>
+        <children>
+          <emfPageIdentifier href="banking.notation#_k7GCMN6aEeKSKuyNBSeryQ"/>
+        </children>
+      </children>
+    </windows>
+  </sashModel>
+</di:SashWindowsMngr>
---
+++ org.modelexecution.fumltesting.examples/test/org/modelexecution/fumltesting/test/ExecutionTest.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright (c) 2013 Vienna University of Technology.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 which accompanies 
+ * this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Tanja Mayerhofer - initial API and implementation
+ */
+package org.modelexecution.fumltesting.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.modelexecution.fumldebug.core.trace.tracemodel.Trace;
+import org.modelexecution.fumldebug.core.trace.tracemodel.ValueInstance;
+import org.modelexecution.fumldebug.papyrus.PapyrusModelExecutor;
+
+import fUML.Semantics.Classes.Kernel.BooleanValue;
+import fUML.Semantics.Classes.Kernel.FeatureValue;
+import fUML.Semantics.Classes.Kernel.IntegerValue;
+import fUML.Semantics.Classes.Kernel.Object_;
+import fUML.Semantics.Classes.Kernel.Value;
+import fUML.Semantics.Classes.Kernel.ValueList;
+import fUML.Semantics.CommonBehaviors.BasicBehaviors.ParameterValueList;
+import fUML.Syntax.Classes.Kernel.Classifier;
+
+public class ExecutionTest {
+
+	@Test
+	public void testBankingExampleCorrectScenario1() {
+		PapyrusModelExecutor executor = new PapyrusModelExecutor("model/banking_correct/banking.di");
+		clearLocus(executor);
+		Trace trace = executor.executeActivity("scenario1", null, null);
+		Assert.assertEquals(4, trace.getActivityExecutions().size());
+		Assert.assertEquals("scenario1", trace.getActivityExecutions().get(0).getActivity().name);
+		ParameterValueList output = executor.getExecutionContext().getActivityOutput(trace.getActivityExecutions().get(0).getActivityExecutionID());
+		Assert.assertEquals(1, output.size());
+		Assert.assertEquals(1, output.get(0).values.size());
+		Assert.assertTrue(((BooleanValue)output.get(0).values.get(0)).value);
+		Object_ accountObject = getObject(trace, "Account");
+		Assert.assertNotNull(accountObject);
+		ValueList accountBalanceValues = getAccountBalance(accountObject, "balance");
+		Assert.assertEquals(1, accountBalanceValues.size());
+		Assert.assertEquals(500, ((IntegerValue)accountBalanceValues.get(0)).value);
+	}
+	
+	@Test
+	public void testBankingExampleCorrectScenario2() {
+		PapyrusModelExecutor executor = new PapyrusModelExecutor("model/banking_correct/banking.di");
+		clearLocus(executor);
+		Trace trace = executor.executeActivity("scenario2", null, null);
+		Assert.assertEquals(4, trace.getActivityExecutions().size());
+		Assert.assertEquals("scenario2", trace.getActivityExecutions().get(0).getActivity().name);
+		ParameterValueList output = executor.getExecutionContext().getActivityOutput(trace.getActivityExecutions().get(0).getActivityExecutionID());
+		Assert.assertEquals(1, output.size());
+		Assert.assertEquals(1, output.get(0).values.size());
+		Assert.assertFalse(((BooleanValue)output.get(0).values.get(0)).value);
+		Object_ accountObject = getObject(trace, "Account");
+		Assert.assertNotNull(accountObject);
+		ValueList accountBalanceValues = getAccountBalance(accountObject, "balance");
+		Assert.assertEquals(1, accountBalanceValues.size());
+		Assert.assertEquals(300, ((IntegerValue)accountBalanceValues.get(0)).value);		
+	}
+	
+	@Test
+	public void testBankingExampleIncorrectScenario1() {
+		PapyrusModelExecutor executor = new PapyrusModelExecutor("model/banking_incorrect/banking.di");
+		clearLocus(executor);
+		Trace trace = executor.executeActivity("scenario1", null, null);
+		Assert.assertEquals(4, trace.getActivityExecutions().size());
+		Assert.assertEquals("scenario1", trace.getActivityExecutions().get(0).getActivity().name);
+		ParameterValueList output = executor.getExecutionContext().getActivityOutput(trace.getActivityExecutions().get(0).getActivityExecutionID());
+		Assert.assertEquals(1, output.size());
+		Assert.assertEquals(1, output.get(0).values.size());
+		Assert.assertTrue(((BooleanValue)output.get(0).values.get(0)).value);
+		Object_ accountObject = getObject(trace, "Account");
+		Assert.assertNotNull(accountObject);
+		ValueList accountBalanceValues = getAccountBalance(accountObject, "balance");
+		Assert.assertEquals(1, accountBalanceValues.size());
+		// error (argument order of subtract method wrong):
+		Assert.assertEquals(-500, ((IntegerValue)accountBalanceValues.get(0)).value);
+	}
+	
+	@Test
+	public void testBankingExampleIncorrectScenario2() {
+		PapyrusModelExecutor executor = new PapyrusModelExecutor("model/banking_incorrect/banking.di");
+		clearLocus(executor);
+		Trace trace = executor.executeActivity("scenario2", null, null);
+		Assert.assertEquals(3, trace.getActivityExecutions().size());
+		Assert.assertEquals("scenario2", trace.getActivityExecutions().get(0).getActivity().name);
+		ParameterValueList output = executor.getExecutionContext().getActivityOutput(trace.getActivityExecutions().get(0).getActivityExecutionID());		
+		Assert.assertEquals(1, output.size());
+		// error (no output provided because pin was invalid):
+		Assert.assertEquals(0, output.get(0).values.size());
+		Object_ accountObject = getObject(trace, "Account");
+		Assert.assertNotNull(accountObject);
+		ValueList accountBalanceValues = getAccountBalance(accountObject, "balance");
+		Assert.assertEquals(1, accountBalanceValues.size());
+		Assert.assertEquals(800, ((IntegerValue)accountBalanceValues.get(0)).value);		
+	}
+
+	@Test
+	public void testBankingExampleIncorrectScenario3() {
+		PapyrusModelExecutor executor = new PapyrusModelExecutor("model/banking_incorrect/banking.di");
+		clearLocus(executor);
+		Trace trace = executor.executeActivity("scenario3", null, null);
+		Assert.assertEquals(4, trace.getActivityExecutions().size());
+		Assert.assertEquals("scenario3", trace.getActivityExecutions().get(0).getActivity().name);
+		ParameterValueList output = executor.getExecutionContext().getActivityOutput(trace.getActivityExecutions().get(0).getActivityExecutionID());
+		Assert.assertEquals(1, output.size());
+		// error (no output provided because balance == amount):
+		Assert.assertEquals(0, output.get(0).values.size());
+		Object_ accountObject = getObject(trace, "Account");
+		Assert.assertNotNull(accountObject);
+		ValueList accountBalanceValues = getAccountBalance(accountObject, "balance");
+		Assert.assertEquals(1, accountBalanceValues.size());
+		// error (balance was not reduced):
+		Assert.assertEquals(800, ((IntegerValue)accountBalanceValues.get(0)).value);
+	}
+	
+	private void clearLocus(PapyrusModelExecutor executor) {
+		executor.getExecutionContext().getLocus().extensionalValues.clear();
+	}
+	
+	private ValueList getAccountBalance(Object_ object, String featurename) {
+		for(FeatureValue fv : object.featureValues) {
+			if(fv.feature.name.equals(featurename)) {
+				return fv.values;
+			}
+		}
+		return null;
+	}
+
+	private Object_ getObject(Trace trace, String classifiername) {
+		for(ValueInstance valueInstance : trace.getValueInstances()) {
+			Value value = valueInstance.getRuntimeValue();
+			if(value instanceof Object_) {
+				Object_ object_ = (Object_) value;
+				for(Classifier c : object_.getTypes()) {
+					if(c.name.equals(classifiername)) {
+						return object_;
+					}
+				}
+			}
+		}
+		return null;
+	}
+}
